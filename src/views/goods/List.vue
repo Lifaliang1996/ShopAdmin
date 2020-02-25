41040bedcabc3d3c60827ef7d7dc37e9c7b3f7cd
Lifaliang <274750134@qq.com>
2020-02-25 21:52:55 +0800

    商品列表

--- src/views/goods/List.vue
+++ src/views/goods/List.vue
@@ -1,12 +1,34 @@
 <template>
-	<div>list</div>
+	<div>
+		<el-table :data="goodsList">
+      <el-table-column type="selection" align="center" width="50"></el-table-column>
+			<el-table-column align="center" width="80" prop="id" label="ID"></el-table-column>
+			<el-table-column align="center" width="100" label="图片">
+				<template slot-scope="scope">
+					<img :src="scope.row.goodsImg" class="goods-img" alt />
+				</template>
+			</el-table-column>
+			<el-table-column align="center" prop="goodsName" label="名称"></el-table-column>
+			<el-table-column align="center" label="价格">
+        <template slot-scope="scope"><span class="price">{{ scope.row.goodsPrice }}</span></template>
+      </el-table-column>
+			<el-table-column align="center" prop="buyNum" label="销量"></el-table-column>
+			<el-table-column align="center" prop="goodsStock" label="库存"></el-table-column>
+			<el-table-column align="center" label="操作">
+        <template slot-scope="scope">
+          <span class="text-button" size="mini">修改</span>
+          <span class="text-button" size="mini">删除</span>
+        </template>
+			</el-table-column>
+		</el-table>
+	</div>
 </template>
 
 <script>
 export default {
 	data() {
 		return {
-			goods: {}
+			goodsList: []
 		};
 	},
 	created() {
@@ -16,11 +38,19 @@ export default {
 				pageSize: 10
 			})
 			.then(res => {
-				console.log(res);
+				this.goodsList = res.list;
 			});
 	}
 };
 </script>
 
 <style>
+.goods-img {
+	width: 40px;
+	height: 40px;
+}
+
+.text-button {
+  margin-right: 5px;
+}
 </style>
